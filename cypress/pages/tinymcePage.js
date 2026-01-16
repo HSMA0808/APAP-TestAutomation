896f3eea60aea5218e2bad702d469b5a9f63a016
HECTOR S. MEDINA ACOSTA <hsma0808@gmail.com>
2026-01-16 14:54:00 -0400

    Update tinymcePage.js

--- cypress/pages/tinymcePage.js
+++ cypress/pages/tinymcePage.js
@@ -1,6 +1,7 @@
 
 class tinymcePage {
     elements ={
+        fraimeLocator: "[title=\"Rich Text Area\"]",
         close_warning_message_button : () => cy.get('[aria-label="Close"]'),
         editor_text_iframe : () => cy.get('#mce_0_ifr').its('0.contentDocument.body'), 
     }
@@ -8,44 +9,44 @@ class tinymcePage {
         this.elements.close_warning_message_button().click();
     }
     async showCurrentEditorTextContent() {
-        cy.frameLoaded('[title="Rich Text Area"]');
+        cy.frameLoaded(this.elements.fraimeLocator);
         this.elements.editor_text_iframe().then( node => { 
             const content = node[0].innerText
             cy.log('Texto actual: ', content)
         }) 
     }
      async modifyEditTextContentValue() {
-        cy.frameLoaded('[title="Rich Text Area"]');
+        cy.frameLoaded(this.elements.fraimeLocator);
         this.elements.editor_text_iframe().find('p').then((node) => {
             node[0].innerText = 'Nuevo valor ingresado'
         }) 
     }
     async alignCenter() {
-        cy.frameLoaded('[title="Rich Text Area"]');
+        cy.frameLoaded(this.elements.fraimeLocator);
         this.elements.editor_text_iframe().then((node) => {
             node[0].style.textAlign = 'center'
         }) 
     }
     async alignRight() {
-        cy.frameLoaded('[title="Rich Text Area"]');
+        cy.frameLoaded(this.elements.fraimeLocator);
         this.elements.editor_text_iframe().then((node) => {
             node[0].style.textAlign = 'right'
         }) 
     }
     async alignLeft() {
-        cy.frameLoaded('[title="Rich Text Area"]');
+        cy.frameLoaded(this.elements.fraimeLocator);
         this.elements.editor_text_iframe().then((node) => {
             node[0].style.textAlign = 'left'
         }) 
     }
     async makeTextBold() {
-         cy.frameLoaded('[title="Rich Text Area"]');
+         cy.frameLoaded(this.elements.fraimeLocator);
         this.elements.editor_text_iframe().then((node) => {
             node[0].style.fontWeight = 'bold'
         }) 
     }
     async changeTextColor() {
-         cy.frameLoaded('[title="Rich Text Area"]');
+         cy.frameLoaded(this.elements.fraimeLocator);
         this.elements.editor_text_iframe().then((node) => {
             node[0].style.color = 'red'
         }) 
